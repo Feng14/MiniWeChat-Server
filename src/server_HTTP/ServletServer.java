e69b761792a97f91d2701668be5dce490243c041
肖泽锋 <383019339@qq.com>
2015-03-27 14:11:43 +0800

    【新功能】httpClient的初步编写改善

--- src/server_HTTP/ServletServer.java
+++ src/server_HTTP/ServletServer.java
@@ -1,4 +1,4 @@
-package tools;
+package server_HTTP;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,18 +13,20 @@ public class ServletServer extends HttpServlet {
 	// 用于处理客户端发送的GET请求 　　
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.err.println("doGet");
-//		response.setContentType("text/html;charset=UTF-8");// 这条语句指明了向客户端发送的内容格式和采用的字符编码．
-//		PrintWriter out = response.getWriter();
-//		out.println(" 您好！");// 利用PrintWriter对象的方法将数据发送给客户端 　　
-//		out.close();
-		
-		response.setContentType("text/html");
+		response.setContentType("text/html;charset=UTF-8");// 这条语句指明了向客户端发送的内容格式和采用的字符编码．
 		PrintWriter out = response.getWriter();
-		out.println("＜html＞＜head＞＜title＞");
-		out.println("This is my first Servlet");
-		out.println("＜/title＞＜/head＞＜body＞");
-		out.println("＜h1＞Hello,World!＜/h1＞");
-		out.println("＜/body＞＜/html＞");
+		out.println("type : " + request.getParameter("type"));
+		out.println("imageName : " + request.getParameter("imageName"));
+//		out.println(" 您好！");// 利用PrintWriter对象的方法将数据发送给客户端 　　
+		out.close();
+		
+//		response.setContentType("text/html");
+//		PrintWriter out = response.getWriter();
+//		out.println("＜html＞＜head＞＜title＞");
+//		out.println("This is my first Servlet");
+//		out.println("＜/title＞＜/head＞＜body＞");
+//		out.println("＜h1＞Hello,World!＜/h1＞");
+//		out.println("＜/body＞＜/html＞");
 	}
 
 	// 用于处理客户端发送的POST请求
