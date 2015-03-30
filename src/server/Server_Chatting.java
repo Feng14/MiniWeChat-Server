f0b47108adb5b2b713c1e0a346bf102c847a92a6
肖泽锋 <383019339@qq.com>
2015-03-30 15:13:31 +0800

    【修改缺陷】发消息的Ptotobuf

---
+++ src/server/Server_Chatting.java
@@ -0,0 +1,15 @@
+package server;
+
+/**
+ * 主服务器下的子服务器，负责处理发消息相关事件
+ * @author Feng
+ */
+public class Server_Chatting {
+	public static Server_Chatting instance = new Server_Chatting();
+	
+	
+	private Server_Chatting(){
+		
+	}
+
+}
