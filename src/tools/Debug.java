20f6f2acfc3e434adb0054c0fbabc83afa66acc7
肖泽锋 <383019339@qq.com>
2015-04-01 10:07:59 +0800

    【修改缺陷】修改Debug类

--- src/tools/Debug.java
+++ src/tools/Debug.java
@@ -5,43 +5,53 @@ import org.apache.log4j.Logger;
 
 public class Debug {
 	public static boolean LogSwitch = true;
+	public static boolean LoggerSwitch = true;
 	static Logger logger = Logger.getLogger(Debug.class);
 
 	public static void init(){
-		if(null == logger){
+		if(logger == null){
 			logger = Logger.getLogger(Debug.class);
 		}
 	}
 	
 	public static void log(String messsage) {
 		init();
-		if (LogSwitch){
+		if (LogSwitch)
 			System.out.println(getTime() + messsage);
+		
+		if (LoggerSwitch)
 			logger.debug(messsage);
-		}
+		
 //			System.out.println(getTime() + messsage);
 	}
 	
 	
 	public static void log(String head, String messsage) {
 		init();
-		if (LogSwitch){
-			logger.debug(getTime() + head + " : " + messsage);
+		if (LogSwitch)
 			System.out.println(getTime() + head + " : " + messsage);
-		}
+		
+		
+		if (LoggerSwitch) 
+			logger.debug(head + " : " + messsage);
 	}
 
 	public static void log(String[] heads, String messsage) {
 		init();
 		if (LogSwitch) {
-			logger.debug(getTime());
-			System.out.println(getTime());
-			for (String s : heads) {
-				logger.debug(s + " : ");
+			System.out.print(getTime());
+			for (String s : heads)
 				System.out.print(s + " : ");
-			}
-			logger.debug(messsage);
+			
 			System.out.println(messsage);
+		}
+		
+		if (LoggerSwitch) {
+			logger.debug(getTime());
+			for (String s : heads)
+				logger.debug(s + " : ");
+			
+			logger.debug(messsage);
 		}
 	}
 	
